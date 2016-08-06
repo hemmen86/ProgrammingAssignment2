04b08db28368889472e49be923d8ec0ff0136502
hemmen86 <hemmen1@gmail.com>
2016-08-06 20:04:16 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,40 @@
-#my file
+#My work
 
-## Put comments here that give an overall description of what your
-## functions do
+##Those function are mention to inverse a matrix. Yet, for saving 
+##time using the 2 functions enable cheking if the calcualtion already done
 
-## Write a short comment describing this function
+## This is the first function which inverse the matrix and provide a list
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve)  invmat <<- solve
+  getinv <- function() invmat
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This is the second function, the input shuold be the output of the
+#first function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    invmat <- x$getinv()
+    if(!is.null(invmat)) {
+      message("getting cached data")
+      return(invmat)
+    }
+    data <- x$get()
+    invmat <- solve(data, ...)
+    x$setinv(invmat)
+    invmat
+  } 
+  
+
+
+
